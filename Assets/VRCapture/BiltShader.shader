07e3b7a9b61f0912d029c6baf30a523ab9f4deb4
zhouqi <153415592@qq.com>
2016-12-06 15:06:42 +0800

    添加文件

---
+++ Assets/VRCapture/BiltShader.shader
@@ -0,0 +1,60 @@
+﻿Shader "VrCapture/BiltShader" {
+	Properties {
+		_MainTex ("Screen Blended", 2D) = "" {}
+	}
+
+	CGINCLUDE
+	#include "UnityCG.cginc"
+	struct v2f {
+		float4 pos : POSITION;
+		float2 uv : TEXCOORD0;
+	};
+	
+	sampler2D _MainTex;
+	float4 _BlitParams;
+		
+	v2f vert(appdata_img v) {
+		v2f o;
+		o.pos = mul(UNITY_MATRIX_MVP, v.vertex);	
+		o.uv = v.texcoord.xy;
+		return o;
+	} 
+	half4 frag(v2f i) : COLOR {
+		float2 uv = i.uv;
+		float steps = _BlitParams.z;
+		float pi = 3.1415926;
+
+		float scale_u = uv.x * steps;
+		float slice = floor(scale_u);		// int [0-360)
+		float slice_u = scale_u - slice; 	// percent in slice [0-1)
+		float scale = cos((uv.y - 0.5) * pi);
+		uv.x = (slice + 0.5 + (slice_u - 0.5) * scale) / steps;
+
+#if 1 // super sampling...
+		float2 invTargetSize = float2(_BlitParams.x, _BlitParams.y);
+		half3 color0 = tex2D(_MainTex, uv + float2(-0.375,  0.125) * invTargetSize).rgb;
+		half3 color1 = tex2D(_MainTex, uv + float2(-0.125, -0.375) * invTargetSize).rgb;
+		half3 color2 = tex2D(_MainTex, uv + float2(0.375, -0.125) * invTargetSize).rgb;
+		half3 color3 = tex2D(_MainTex, uv + float2(0.125,  0.375) * invTargetSize).rgb;
+		half3 color = (color0 + color1 + color2 + color3) * 0.25;
+		return half4(color, 1);
+#else
+		return half4(tex2D(_MainTex, uv).rgb, 1);
+#endif
+	}
+	ENDCG 
+	
+	Subshader {
+	 Pass {
+		  ZTest Always Cull Off ZWrite Off
+		  Fog { Mode off }      
+
+		  CGPROGRAM
+		  //#pragma fragmentoption ARB_precision_hint_fastest 
+		  #pragma vertex vert
+		  #pragma fragment frag
+		  ENDCG
+	  }
+	}
+	Fallback off
+}
---
+++ Assets/VRCapture/StereoCapture.cs
@@ -0,0 +1,319 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class StereoCapture : MonoBehaviour
+{
+    const float HorizontalAngularIncrement = 1.0f;
+    const float VerticalAngularIncrement = 90.0f;
+    const float EyeSeparation = 0.032f;
+    const float NearClipPlane = 0.1f;
+    const int CaptureWidth = 2048;
+    const int CaptureHeight = 1024;
+    const int CaptureQuality = 75;
+    const int BuffFrame = 2;
+
+    public Material BiltMaterial;
+    public string CaptureFolder = "d:\\Captures";
+    public string EnableEffects;
+    public int StartFrame = 0;
+    public int EndFrame = -1;
+    public int CaptureFrameRate = 60;
+    public int Port = 3069;
+    public bool CaptureStart;
+
+    int mFrameIdx = 0;
+    Camera mCaptureCamera;
+    GameObject mCameraRoot;
+    SimpleTcpServer mTcpServer;
+    RenderFrame[] mRenderFrames;
+    Texture2D mResolveTexture;
+
+    class RenderFrame
+    {
+        public RenderTexture RenderTargetLeft;
+        public RenderTexture RenderTargetRight;
+        public Texture2D ResolveTexture;
+        public int FrameCount;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        mTcpServer = new SimpleTcpServer();
+        mTcpServer.StartAccept(Port, StartCapture, StopCapture);
+    }
+
+    void OnDestroy()
+    {
+        if (mCameraRoot)
+            Destroy(mCameraRoot);
+        mCameraRoot = null;
+
+        // we share the same texture.
+        if (mResolveTexture)
+            GameObject.Destroy(mResolveTexture);
+        mResolveTexture = null;
+
+        if (mRenderFrames != null)
+        {
+            foreach (var frame in mRenderFrames)
+            {
+                GameObject.Destroy(frame.RenderTargetLeft);
+                GameObject.Destroy(frame.RenderTargetRight);
+            }
+            mRenderFrames = null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CaptureStart)
+            CaptureFrame();
+    }
+
+    void StartCapture(int cubeSize, int fps)
+    {
+        CaptureStart = true;
+    }
+
+    void StopCapture()
+    {
+        CaptureStart = false;
+    }
+
+    void InitCapture()
+    {
+        //  alreay initialized.
+        if (mCameraRoot)
+            return;
+
+        // capture started.
+        mCameraRoot = new GameObject("CameraRoot");
+        mCaptureCamera = mCameraRoot.AddComponent<Camera>();
+        mCaptureCamera.CopyFrom(Camera.main);
+        mCaptureCamera.nearClipPlane = NearClipPlane;
+
+        // setup the capture mode.
+        Time.captureFramerate = CaptureFrameRate;
+
+        mResolveTexture = new Texture2D(CaptureWidth, CaptureHeight * 2, TextureFormat.ARGB32, false);
+
+        // new the buffers.
+        mRenderFrames = new RenderFrame[BuffFrame];
+        for (var i = 0; i < BuffFrame; i++)
+        {
+            mRenderFrames[i] = new RenderFrame() {
+                RenderTargetLeft = new RenderTexture(CaptureWidth, CaptureHeight, 0),
+                RenderTargetRight = new RenderTexture(CaptureWidth, CaptureHeight, 0),
+                ResolveTexture = mResolveTexture,
+                FrameCount = -1
+            };
+        }
+    }
+
+    void CaptureFrame()
+    {
+        var frameCount = Time.frameCount;
+        if (frameCount < StartFrame || (EndFrame >= 0 && frameCount > EndFrame + 1))
+            return;
+
+        InitCapture();
+
+        // dump current frame (rendered).
+        var curFrame = mRenderFrames[(mFrameIdx++) % mRenderFrames.Length];
+        if (curFrame.FrameCount > 0)
+        {
+            DumpFrame(curFrame,
+                CaptureFolder,
+                mTcpServer,
+                CaptureQuality,
+                false);
+        }
+
+        // capture to next frame.
+        curFrame.FrameCount = frameCount;
+        CaptureFrame(curFrame,
+            CaptureWidth,
+            CaptureHeight,
+            HorizontalAngularIncrement,
+            VerticalAngularIncrement,
+            EyeSeparation,
+            EnableEffects,
+            mCaptureCamera,
+            transform.position,
+            BiltMaterial);
+    }
+
+    static void DumpFrame(
+        RenderFrame frame,
+        string captureFolder,
+        SimpleTcpServer tcpServer,
+        int quality,
+        bool losslessPNG)
+    {
+        // dump the left.
+        RenderTexture.active = frame.RenderTargetLeft;
+        frame.ResolveTexture.ReadPixels(new Rect(0, 0, frame.RenderTargetLeft.width, frame.RenderTargetLeft.height), 0, 0);
+
+        // dump the right.
+        RenderTexture.active = frame.RenderTargetRight;
+        frame.ResolveTexture.ReadPixels(new Rect(0, 0, frame.RenderTargetRight.width, frame.RenderTargetRight.height), 0, frame.RenderTargetLeft.height);
+
+        RenderTexture.active = null;
+        frame.ResolveTexture.Apply();
+        if (tcpServer != null && tcpServer.Capturing)
+        {
+            var pixels32 = frame.ResolveTexture.GetRawTextureData();
+            tcpServer.Send(frame.FrameCount,
+                frame.ResolveTexture.width,
+                frame.ResolveTexture.height,
+                pixels32);
+        }
+        else if (!string.IsNullOrEmpty(captureFolder))
+        {
+            // write to file.
+            var fileName = string.Format("frame_{0}.{1}", frame.FrameCount.ToString("D5"), losslessPNG ? "png" : "jpg");
+            File.WriteAllBytes(
+                Path.Combine(captureFolder, fileName),
+                losslessPNG ? frame.ResolveTexture.EncodeToPNG() : frame.ResolveTexture.EncodeToJPG(quality));
+        }
+    }
+
+    static void CaptureFrame(
+        RenderFrame frame,
+        int captureWidth,
+        int captureHeight,
+        float horizontalInc,
+        float verticalInc,
+        float eyeSeparation,
+        string effects,
+        Camera camera,
+        Vector3 capturePos,
+        Material biltMaterial)
+    {
+        var cameraTrans = camera.transform;
+        var horizontalSteps = (int)(360.0f / horizontalInc);
+        var verticalSteps = (int)(180.0f / verticalInc + 1);
+
+        camera.fieldOfView = 180.0f / verticalSteps;
+        biltMaterial.SetVector("_BlitParams", new Vector4(
+            1.0f / captureWidth,
+            1.0f / captureHeight,
+            horizontalSteps,
+            verticalSteps));
+
+        for (var i = 0; i < 2; i++)
+        {
+            var left = (i == 0);
+            var eyeOffset = new Vector3(left ? -eyeSeparation : eyeSeparation, 0, 0);
+
+            var renderTarget = RenderTexture.GetTemporary(captureWidth * 2, captureHeight * 2, 16, RenderTextureFormat.ARGB32);
+            var sliderWidth = (float)renderTarget.width / horizontalSteps;
+            var sliderHeight = (float)renderTarget.height / verticalSteps;
+
+            // render the slides to the target.
+            RenderTexture.active = renderTarget;
+            camera.targetTexture = renderTarget;
+            for (var row = 0; row < verticalSteps; row++)
+            {
+                for (var col = 0; col < horizontalSteps; col++)
+                {
+                    cameraTrans.rotation = Quaternion.Euler(
+                        180.0f * ((verticalSteps / 2) - row) / verticalSteps,
+                        360.0f * col / horizontalSteps,
+                        0);
+                    cameraTrans.position = capturePos + cameraTrans.rotation * eyeOffset;
+                    // build the viewport.
+                    var rect = new Rect(col * sliderWidth, row * sliderHeight, sliderWidth, sliderHeight);
+                    camera.pixelRect = rect;
+                    camera.Render();
+                }
+            }
+            camera.targetTexture = null;
+
+            // slice to SphericalAtlas
+            var sphericalAtlas = RenderTexture.GetTemporary(captureWidth, captureHeight);
+            Graphics.Blit(renderTarget, sphericalAtlas, biltMaterial);
+            RenderTexture.ReleaseTemporary(renderTarget);
+
+            // process the full screen effects.
+            PostProcessEffects(ref sphericalAtlas, effects);
+
+            // read the pixels to texture..
+            RenderTexture.active = sphericalAtlas;
+            Graphics.Blit(sphericalAtlas, left ? frame.RenderTargetLeft : frame.RenderTargetRight);
+
+            // no need.
+            RenderTexture.active = null;
+            RenderTexture.ReleaseTemporary(sphericalAtlas);
+        }
+    }
+
+    public static void PostProcessEffects(ref RenderTexture src, string componentNames)
+    {
+        RenderTexture des = null;
+        var components = componentNames.Split(" ;,".ToCharArray(), System.StringSplitOptions.RemoveEmptyEntries);
+        for (var i = 0; i < components.Length; i++)
+        {
+            var component = Camera.main.GetComponent(components[i]);
+            if (component == null)
+                continue;
+
+            var renderMethod = component.GetType().GetMethod("OnRenderImage");
+            if (renderMethod == null)
+                continue;
+
+            if (!des) des = RenderTexture.GetTemporary(src.width, src.height);
+
+            renderMethod.Invoke(component, new object[] { src, des });
+            var tmp = des;
+            des = src;
+            src = tmp;
+        }
+
+        if (des)
+            RenderTexture.ReleaseTemporary(des);
+    }
+
+    [ContextMenu("Capture")]
+    void ExecuteCapture()
+    {
+        var cameraRoot = new GameObject("CameraRoot");
+        var camera = cameraRoot.AddComponent<Camera>();
+        camera.CopyFrom(Camera.main);
+        camera.nearClipPlane = NearClipPlane;
+        
+        var frame = new RenderFrame() {
+            RenderTargetLeft = new RenderTexture(CaptureWidth, CaptureHeight, 0),
+            RenderTargetRight = new RenderTexture(CaptureWidth, CaptureHeight, 0),
+            ResolveTexture = new Texture2D(CaptureWidth, CaptureHeight * 2),
+            FrameCount = 0
+        };
+
+        CaptureFrame(
+            frame,
+            CaptureWidth,
+            CaptureHeight,
+            HorizontalAngularIncrement,
+            VerticalAngularIncrement,
+            EyeSeparation,
+            EnableEffects,
+            camera,
+            transform.position,
+            BiltMaterial);
+
+        DumpFrame(
+            frame,
+            CaptureFolder,
+            null,
+            0,
+            true);
+
+        DestroyImmediate(frame.RenderTargetLeft);
+        DestroyImmediate(frame.RenderTargetRight);
+        DestroyImmediate(frame.ResolveTexture);
+        DestroyImmediate(cameraRoot);
+    }
+}
